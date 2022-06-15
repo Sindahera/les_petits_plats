36af7fc21d0169adaf6649c0a9c3e82d426cc62f
Sindahera <sindaherarichard@gmail.com>
2022-06-15 17:06:03 +0200

    search-bar and search-result

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ window.customElements.define("index-page", IndexPage);
 
 import{ApplianceSelect} from "./pages/components/appliance-select.js"
 window.customElements.define("appliance-select", ApplianceSelect);
+import {SearchBar} from "./pages/components/search-bar.js"
+window.customElements.define("search-bar", SearchBar);
+import {SearchResult} from "./pages/components/search-result.js"
+window.customElements.define("search-result", SearchResult);
 import{UstensilSelect} from "./pages/components/ustensil-select.js"
 window.customElements.define("ustensil-select", UstensilSelect);
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  purge: ["./*.js", "./pages/*.js"],
+  purge: ["./*.js", "./pages/*.js", "./pages/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
